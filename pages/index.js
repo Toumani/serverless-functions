21e70c6e0fbf77c863fff9f0b1dd2bd13928d462
Toumani <toumani49@gmail.com>
2020-08-01 23:56:36 +0100

    Date.py

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+class Home extends React.Component {
+  render () {
+    return (
+      <h1>Hello world</h1>
+    )
+  }
+}
+
+export default Home;
